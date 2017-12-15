4beed2d8d6932ea080ac236aff5ac4dd307c8115
Macovei TUDOR <mtudor@e1p39.cluj.42.fr>
2017-12-15 17:43:33 +0200

    added tetra_size & defines for matrix sizes

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,7 +6,7 @@
 /*   By: bavram <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 17:02:44 by bavram            #+#    #+#             */
-/*   Updated: 2017/12/15 17:24:00 by bavram           ###   ########.fr       */
+/*   Updated: 2017/12/15 17:41:16 by mtudor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,27 @@
 
 int	main(int argc, char **args)
 {
-	t_tetramino	tetra[26];
-	char		fillit_output[100][100];
+	t_tetramino	tetra[TETRA_MAX];
+	char		fillit_output[FILLIT_MAX][FILLIT_MAX];
 	int			output_size;
-	
+	int			tetra_size;
+
 	if (argc != 2)
 	{
 		show_usage();
 		return (1);
 	}
-	if (read_tetra(args[1], tetra))
+	if (read_tetra(args[1], tetra, &tetra_size))
 	{
 		ft_putstr("error\n");
 		return (1);
 	}
-	if (!valid_tetra(tetra))
+	if (!valid_tetra(tetra, tetra_size))
 	{
 		ft_putstr("error\n");
 		return (1);
 	}
-	output_size = solve_tetra(tetra, fillit_output);
+	output_size = solve_tetra(tetra, fillit_output, tetra_size);
 	print_fillit(fillit_output, output_size);
 	return (0);
 }
--- fillit/fillit.h
+++ fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: bavram <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 16:50:42 by bavram            #+#    #+#             */
-/*   Updated: 2017/12/15 17:21:35 by bavram           ###   ########.fr       */
+/*   Updated: 2017/12/15 17:42:07 by mtudor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "../libft/libft.h"
 # include <fcntl.h>
+# define TETRA_MAX 26
+# define FILLIT_MAX 100
 
 typedef struct	s_t{
 	char		row[4];
