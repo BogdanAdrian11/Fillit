5fecf72da7b6bf3451cd513bdff29503b1190d03
Macovei TUDOR <mtudor@e1p60.cluj.42.fr>
2017-12-20 18:30:18 +0200

    try new solve

--- fillit/solve_tetra.c
+++ fillit/solve_tetra.c
@@ -6,7 +6,7 @@
 /*   By: mtudor <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 16:24:56 by mtudor            #+#    #+#             */
-/*   Updated: 2017/12/19 19:25:39 by bavram           ###   ########.fr       */
+/*   Updated: 2017/12/20 18:27:27 by mtudor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,63 @@ static void	char_change(t_fout *out, char c1, char c2)
 	}
 }
 
-static int	place_tetra(t_tetramino tetra, t_fout *out, int i, int j)
+static int	place_tetra(t_tetramino tetra, t_fout *out, int i, int j)	//returns 1 if the place is possible, then places it
 {
-	return (1);
+	int		p;
+	int		q;
+	int		count;
+
+	count = 0;
+	p = 0;
+	q = 0;
+	while (tetra.matrix[p][q] != '#' && p != 4)
+		if(q == 3)
+		{
+			p++;
+			q = 0;
+		}
+		else
+			q++;
+	while (p != 4)		//i si j sunt gresite rau
+	{
+		if (((*out).matrix[i][j] != '.' || i >= (*out).size || j >= (*out).size)
+				&& tetra.matrix[p][q] == '#')
+			return (0);
+		if (tetra.matrix[p][q] == '#')
+		{
+			(*out).matrix[i][j] = tetra.matrix[p][q];
+			//ft_putnbr(i);
+			//ft_putnbr(j);
+			//ft_putchar(' ');
+			count++;
+		}
+		if (q == 3)
+		{
+			i++;
+			p++;
+			q = 0;
+			j = j - 3;
+		}
+		else
+		{
+			q++;
+			j++;
+		}
+	}
+	if (count == 4)
+	{
+		//ft_putchar('A');
+		return (1);
+	}
+	return (0);
 }
 
 static int	solve(t_tetramino *tetra, t_fout *out, int k, int tetra_size)
 {
 	int		i;
 	int		j;
-
+	
+	ft_putnbr(k);
 	if (k == tetra_size)
 		return (1);
 	i = 0;
@@ -62,11 +109,15 @@ static int	solve(t_tetramino *tetra, t_fout *out, int k, int tetra_size)
 			if (place_tetra(tetra[k], out, i, j))
 			{
 				char_change(out, '#', 'A' + k);
+				ft_putchar('\n');
+				//print_fillit(*out);
 				return (solve(tetra, out, k + 1, tetra_size));
 			}
+			char_change(out, '#', 'A' + k);
 			j++;
 		}
 		i++;
+		//ft_putnbr(i);
 	}
 	char_change(out, 'A' + k, '.');
 	return (0);
@@ -76,5 +127,14 @@ void		solve_tetra(t_tetramino *tetra, int tetra_size, t_fout *out)	//crestem ite
 {
 	(*out).size = start_size(tetra_size);
 	while (!solve(tetra, out, 0, tetra_size))
+	{
 		(*out).size++;
+		init_output(out);
+		//print_fillit(*out);
+	}
+	//(*out).size = 4;
+	//place_tetra(tetra[0], out, 2, 0);
 }
+
+//poti sa te folosesti de out ca si copie si nu trebuie sa deletezi ce ai pus
+//k trebuie sa fie in toate permutarile posibile, nu numai cresacator
